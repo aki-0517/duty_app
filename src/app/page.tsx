d5bd61d68db1d8513c6615c13e44bfcc0f3eeb58
Aki-8717 <a-ono@digeon.co>
2023-07-28 23:17:05 +0900

    エラーハンドリング

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
-export default function Home() {
+import { Heading } from "./common/components";
+import type { Article } from "./types";
+
+async function getArticles() {
+  const res = await fetch("http:", {
+  cache: "no-store",
+});
+
+  // エラーハンドリング
+  if (!res.ok) {
+    throw new Error("Failed to fetch articles");
+  }
+
+  const data = await res.json();
+  return data.articles as Article[];
+}
+
+export default async function Home() {
+  const articles = await getArticles();
+
   return (
     <div>
-      <h1>新着記事</h1>
-      <ul>
-        <li>記事1</li>
-        <li>記事2</li>
-        <li>記事3</li>
-      </ul>
+      <Heading as="h1" mb={4}>
+        新着記事
+      </Heading>
     </div>
-  )
+  );
 }
